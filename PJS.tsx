5baa5b2739d7406d205b74bc3c050116d416af78
erwidosantoss <peterjaya811@gmail.com>
2026-03-06 07:52:16 +0700

    Update PJS.tsx

--- PJS.tsx
+++ PJS.tsx
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-const message = "Nyari apa heh"
-const message2 = "nyari keadilan negara"
-
-//Nanti chat disini ya
-=======
-const message = "lu update github"
->>>>>>> f4ed3fb36b5bc00bf366feface19e75c86de6866
+//test
