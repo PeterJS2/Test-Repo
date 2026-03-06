2f06b94f558d9de717b9e24cf04cc1a63ee679db
PeterJS2 <peterjaya811@gmail.com>
2026-03-06 10:22:05 +0700

    Update PJS.tsx

--- PJS.tsx
+++ PJS.tsx
@@ -1,2 +1,2 @@
 const pesan1 = "win itu perlu screenshot dari postmannya ga tuh yang bagian pertama?"
-const pesan2 = "yakin bisa selesai ga sebelum jam 12?"
+const pesan2 = "yakin bisa selesai ga sebelum jam 12? soalnya si ko hans sendiri bilang 30 menit kelar ngoding"
